554ff4fb31333367100487b42ee4f2aa4abb3ba3
Steven Feldshaw <gregoryfightsons@gmail.com>
2023-11-29 12:13:49 -0500

    Added Book Structure and Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do  
-  resources :libraries, only: [:index, :create]
+Rails.application.routes.draw do
+  resources :libraries, only: [:index, :create] do
+    resources :books, only: [:index, :create] do
+      post '/search', to: 'books#search', on: :collection
+    end
+  end
 end
