d005308739a1b7fe4c68ba78ea9adfae29734f30
Steven Feldshaw <gregoryfightsons@gmail.com>
2023-11-29 13:31:04 -0500

    Added Borrower Structure, Functionality and Associated Tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :libraries, only: [:index, :create] do
+    resources :borrowers, only: [:index, :create] do
+      member do
+        post :lend_book
+        post :return_book
+      end
+    end
+
     resources :books, only: [:index, :create] do
       post '/search', to: 'books#search', on: :collection
     end
