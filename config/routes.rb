15db1ad12b243be2a08692d53e4e5e08d307b0a0
Steven Feldshaw <gregoryfightsons@gmail.com>
2023-11-29 14:03:12 -0500

    Added JWT Auth and LibraryAdmin Structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  
+  resources :library_admins, param: :_username
+  
   resources :libraries, only: [:index, :create] do
     resources :borrowers, only: [:index, :create] do
       member do
